d7ff47079fb8f399b3a79f0a81471af44e53ecf5
josehb96 <josehurtadobaeza@gmail.com>
2025-04-16 00:24:07 +0200

    fix: update SCORE_API_ENDPOINT context from client to server in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: "server", access: "public"}),
-      SCORE_API_ENDPOINT: envField.string({ context: "client", access:"public" })
+      SCORE_API_ENDPOINT: envField.string({ context: "server", access:"public" })
     }
   }
 });
